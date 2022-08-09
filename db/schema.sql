7fe7568db753f4ff1b7c8adafafea83bf3202b18
Josh <josh.haddix598@gmail.com>
2022-08-09 16:12:48 -0500

    created votes table

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@ use election;
 drop table if exists candidates;
 drop table if exists parties; 
 drop table if exists voters;
+drop table if exists votes;
 
 create table parties (
     id integer auto_increment primary key,
@@ -16,7 +17,7 @@ create table candidates (
     last_name varchar(30) not null,
     party_id integer,
     industry_connected boolean not null,
-    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+    constraint fk_party foreign key (party_id) references parties(id) on delete set null
 );
 
 create table voters (
@@ -25,4 +26,14 @@ create table voters (
     last_name varchar(30) not null,
     email varchar(50) not null,
     created_at datetime default current_timestamp
+);
+
+create table votes (
+    id integer auto_increment primary key,
+    voter_id integer not null,
+    candidate_id integer not null,
+    created_at datetime default current_timestamp,
+    constraint uc_voter unique (voter_id),
+    constraint fk_voter foreign key (voter_id) references voters(id) on delete cascade,
+    constraint fk_candidate foreign key (candidate_id) references candidates(id) on delete cascade
 );
