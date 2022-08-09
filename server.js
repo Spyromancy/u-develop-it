ecb85f2c761e524f86bf3315a09af15bde352036
Josh <josh.haddix598@gmail.com>
2022-08-09 12:46:08 -0500

    connected database

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'M4r4th15BAE',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 
 app.get('/', (req,res) => {
     res.json({
@@ -14,7 +27,9 @@ app.get('/', (req,res) => {
 
 
 
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req,res) => {
     res.status(404).end();
