e2efce8f813b3e43e7414b79f434da61d89bec69
Josh <josh.haddix598@gmail.com>
2022-08-09 14:16:42 -0500

    created parties table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,9 @@ create table candidates (
     last_name varchar(30) not null,
     industry_connected boolean not null
 );
+
+create table parties (
+    id integer auto_increment primary key,
+    name varchar(50) not null,
+    description text
+);
